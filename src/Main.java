8f74ff8cf11993c8a44c13c538606eef849ced2f
Mir Saleh <you@example.com>
2025-06-19 22:58:05 +0400

    if/else and swithc/case and for task

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,60 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main{
     public static void main(String[] args) {
-    int a=3;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(a==1?"Yanvar":a==2?"Fevral":a==3?"MArt":a==4?"Aprel":a==5?"may":a==6?"iyun":a==7?"iyul":a==8?"avqust":a==9?"sentyabr":a==10?"oktyabr":a==11?"Noyabr":a==12?"dekabr":"1-12 araliqinda reqem secin");
-    int b=a;
-        System.out.println(b==1||b==2||b==12?"Qis":b > 2 && b < 6?"Yaz":b>5 && b<10?"Yay":b>9 && b<12?"payiz":"Duz gun reqem qeyd edin");
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+       double d= (a+b+c)/3;
+        System.out.println("Task:1");
+        System.out.println(d);
+        if(a>40 && b>40 && c>40){
+            if(d>85 && a>85 && b>85 && c>85){
+                System.out.println("Təbriklər! Siz Mükəmməllik Sertifikatı qazandınız");
+            } else if(d>=70 && d<85 && a>60 && b>60 && c>60){
+                System.out.println("Əhsən! Siz Nailiyyət Sertifikatı qazandınız.");
+
+            }else if(d>=50 && d<70){
+                System.out.println("Siz keçdiniz");
+
+        }
+            else{
+                System.out.println("Kəsildiniz.");
+            }
+        }
+        else{
+            System.out.println("Kəsildiniz - Bir və ya bir neçə imtahan balı çox aşağıdır.");
+        }
+        System.out.println("Task:2");
+        int f=scanner.nextInt();
+        switch (f){
+            case 1:
+                System.out.println("Şokolad - $2.00");break;
+            case 2:
+                System.out.println("Soda - $1.50");break;
+            case 3:
+                System.out.println("Çips - $1.20");break;
+            case 4:
+                System.out.println("Sendviç - $3.00");break;
+            case 5:
+                System.out.println("Su - $1.00");break;
+            default:
+                System.out.println("Yanlış seçim. Zəhmət olmasa 1 ilə 5 arasında bir nömrə daxil edin.");break;
+        }
+        System.out.println("Task:3");
+        int u=scanner.nextInt();
+        for(;u<11;u++){
+            for(int i=0;i<11;i++){
+                int l=u*i;
+                System.out.println(u+" x "+i+" = "+l);
+            }
+
+        }
+
 
         }
     }
