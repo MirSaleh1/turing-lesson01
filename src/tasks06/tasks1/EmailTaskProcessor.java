3f877687c3938c49c23e8cba50f1308d97bbf5ca
Mir Saleh <you@example.com>
2025-08-24 22:07:04 +0400

    EmailTaskProcessor class

---
+++ src/tasks06/tasks1/EmailTaskProcessor.java
@@ -0,0 +1,9 @@
+package tasks06.tasks1;
+
+public class EmailTaskProcessor extends AbstractTaskProcessor {
+
+    @Override
+    public void processTask(Task task) {
+        System.out.println("Email sent for task " + task);
+    }
+}
