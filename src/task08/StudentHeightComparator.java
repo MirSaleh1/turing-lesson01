a09d3606aa6befb9f173548fff6470fa2b919852
Mir Saleh <ndnvsaleh123@gmail.com>
2025-09-01 19:51:36 +0400

    Student Height comparator class

---
+++ src/task08/StudentHeightComparator.java
@@ -0,0 +1,14 @@
+package task08;
+
+import java.util.Comparator;
+
+public class StudentHeightComparator implements Comparator<Student> {
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        return  Double.compare(o1.getHeight(), o2.getHeight());
+    }
+
+
+
+}
